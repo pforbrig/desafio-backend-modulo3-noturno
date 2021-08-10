3b84ccb2c5d2c12986a9feb49bab783e2ee301a6
pforbrig <pedroforbrig@gmail.com>
2021-08-10 19:51:15 -0300

    Parte obrigatória e middleware finalizados

--- controladores/produtos.js
+++ controladores/produtos.js
@@ -1,15 +1,26 @@
 const conexao = require('../conexao');
 
 const listarProdutos = async (req, res) => {
+    const { categoria } = req.query;
+
     try {
-        const query = `select * from produtos where usuario_id = $1`;
-        const produtosCadastrados = await conexao.query(query, [req.usuario.id]);
+        if (categoria) {
+            const query = `select * from produtos where usuario_id = $1 and categoria = $2`;
+            const produtosCadastrados = await conexao.query(query, [req.usuario.id, categoria]);
+            if (produtosCadastrados.rowCount === 0) {
+                return res.status(200).json('Você não tem produtos dessa categoria cadastrados.');
+            }
 
-        if (produtosCadastrados.rowCount === 0) {
-            return res.status(200).json('Você não tem produtos cadastrados.');
+            return res.status(200).json(produtosCadastrados.rows);
+        } else {
+            const query = `select * from produtos where usuario_id = $1`;
+            const produtosCadastrados = await conexao.query(query, [req.usuario.id]);
+            if (produtosCadastrados.rowCount === 0) {
+                return res.status(200).json('Você não tem produtos cadastrados.');
+            }
+
+            return res.status(200).json(produtosCadastrados.rows);
         }
-
-        return res.status(200).json(produtosCadastrados.rows);
 
     } catch (error) {
         return res.status(400).json(error.message);
@@ -71,6 +82,47 @@ const cadastrarProduto = async (req, res) => {
     }
 }
 const atualizarProduto = async (req, res) => {
+    const { nome, estoque, preco, descricao, imagem, categoria } = req.body;
+
+    try {
+        const produtoBuscado = await conexao.query('select * from produtos where id = $1', [req.params.id]);
+
+        if (produtoBuscado.rowCount === 0) {
+            return res.status(404).json('Não foi encontrado nenhum produto com o id informado.');
+        }
+        if (produtoBuscado.rows[0].usuario_id !== req.usuario.id) {
+            return res.status(400).json('Você não tem acesso a esse produto.');
+        }
+        if (!nome) {
+            return res.status(400).json('O nome é obrigatório.')
+        }
+        if (!estoque) {
+            return res.status(400).json('A quantidade em estoque é obrigatória.')
+        }
+        if (!preco) {
+            return res.status(400).json('O preço é obrigatório.')
+        }
+        if (!descricao) {
+            return res.status(400).json('A descrição é obrigatória')
+        }
+        if (isNaN(Number(estoque))) {
+            return res.status(400).json('A quantidade em estoque deve ser um número.');
+        }
+        if (isNaN(Number(preco))) {
+            return res.status(400).json('O preço deve ser um número.');
+        }
+        const query = 'update produtos set nome = $1, estoque = $2, preco = $3, descricao = $4, imagem = $5, categoria = $6 where id = $7';
+        const produtoAtualizado = await conexao.query(query, [nome, estoque, preco, descricao, imagem, categoria, req.params.id]);
+
+        if (produtoAtualizado.rowCount === 0) {
+            return res.status(404).json('Não foi possível atualizar o produto.');
+        }
+
+        return res.status(200).json('O produto foi atualizado com sucesso.');
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 const excluirProduto = async (req, res) => {
     try {
@@ -82,14 +134,14 @@ const excluirProduto = async (req, res) => {
         if (produtoBuscado.rows[0].usuario_id !== req.usuario.id) {
             return res.status(400).json('Você não tem acesso a esse produto.');
         }
-        const query = 'delete from usuarios where id = $1';
+        const query = 'delete from produtos where id = $1';
         const produtoExcluido = await conexao.query(query, [req.params.id]);
 
         if (produtoExcluido.rowCount === 0) {
-            return res.status(404).json('Não foi possível excluir o pokemon');
+            return res.status(404).json('Não foi possível excluir o produto.');
         }
 
-        return res.status(200).json('O pokemon foi excluido com sucesso.');
+        return res.status(200).json('O produto foi excluido com sucesso.');
 
     } catch (error) {
         return res.status(400).json(error.message);
